0cf36389ef7e175cc9c0a4265cdfb1d412fdf636
Piyush <Piyush.Dharkar@gmail.com>
2018-02-02 19:09:05 +0530

    Completed basic GUI.

---
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -0,0 +1,16 @@
+package com.example.calculator
+
+import java.util.*
+
+
+
+
+class Calculator(infix: String) {
+
+    private val infix = infix
+
+    fun getAnswer(): String {
+        return ""
+    }
+
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.calculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private fun getChar(view: View) = when (view.id) {
+        R.id.oneButton -> "1"
+        R.id.twoButton -> "2"
+        R.id.threeButton -> "3"
+        R.id.fourButton -> "4"
+        R.id.fiveButton -> "5"
+        R.id.sixButton -> "6"
+        R.id.sevenButton -> "7"
+        R.id.eightButton -> "8"
+        R.id.nineButton -> "9"
+        R.id.zeroButton -> "0"
+
+        R.id.plus -> "+"
+        R.id.minus -> "-"
+        R.id.multiply -> "x"
+        R.id.divide -> "/"
+        R.id.equalTo -> "="
+
+        else -> "Error"
+    }
+
+
+    private fun setButtonOnClickListeners() {
+        val addCharToTextView = {view: View -> textView.text = textView.text.toString() + getChar(view)}
+
+        oneButton.setOnClickListener(addCharToTextView)
+        twoButton.setOnClickListener(addCharToTextView)
+        threeButton.setOnClickListener(addCharToTextView)
+        fourButton.setOnClickListener(addCharToTextView)
+        fiveButton.setOnClickListener(addCharToTextView)
+        sixButton.setOnClickListener(addCharToTextView)
+        sevenButton.setOnClickListener(addCharToTextView)
+        eightButton.setOnClickListener(addCharToTextView)
+        nineButton.setOnClickListener(addCharToTextView)
+        zeroButton.setOnClickListener(addCharToTextView)
+
+        plus.setOnClickListener(addCharToTextView)
+        minus.setOnClickListener(addCharToTextView)
+        multiply.setOnClickListener(addCharToTextView)
+        divide.setOnClickListener(addCharToTextView)
+
+        equalTo.setOnClickListener({textView.text = Calculator(textView.text.toString()).getAnswer()})
+
+        clearButton.setOnClickListener({textView.text = ""})
+        backspaceButton.setOnClickListener({textView.text = if (textView.text != "")
+                textView.text.toString().substring(0, textView.text.toString().length - 1)
+        else
+            ""})
+
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setButtonOnClickListeners()
+
+    }
+}
